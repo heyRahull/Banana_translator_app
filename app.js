512f6a2a2cece6d6c66cf752bf79c7b39992cf4a
Rahul Agarwal <rahul123agarwal99@gmail.com>
2021-04-14 10:14:33 +0530

    added events click and change

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+
 
 btnTranslate.addEventListener("click",()=>{
    console.log("Button Clicked");
+   console.log(txtInput.value);
+})
+
+
+txtInput.addEventListener("change",()=>{return console.log(txtInput.value);
 })
